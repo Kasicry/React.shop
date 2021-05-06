e0854148f7695c2e760464b66b0730f88a844388
Geon <akrmak12@naver.com>
2021-05-06 18:07:24 +0900

    2021-05-06 shop study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import { Button, Navbar, Nav, NavDropdown, Jumbotron } from 'react-bootstrap';
+import Data from './data.js'
+import {Link, Route, Switch} from 'react-router-dom';
+import Detail from './Detail'
 import './App.css';
 
+
 function App() {
+
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Shoe shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home" as={Link} to="/">Home</Nav.Link>
+            <Nav.Link href="#link" as={Link} to="/detail">Detail</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>  
+        <Button variant="outline-success">Search</Button>  
+        </Navbar.Collapse>
+      </Navbar>
+
+
+
+      <Route exact path = "/">
+        <Jumbotron className="background">
+          <h1>20% Season OFF</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Jumbotron>
+
+        <div className="container">
+            <div className="row">
+              {
+                shoes.map((a,i)=>{
+                  return <Card shoes = {a} i={i} />
+                  // return <Card shoes = {shoes[i]} />
+                })
+              }          
+              
+            
+            </div>
+        </div>
+
+      </Route>
+
+      <Route path = "/detail/:id">
+
+      
+
+
+        <Detail shoes={shoes} />
+
+
+      </Route>
+
+
+      
+      
     </div>
-  );
+  )
+}
+
+
+
+
+function Card(props){
+  return(
+      <div className="col-md-4">
+          <img src={"https://codingapple1.github.io/shop/shoes"+(props.i+1)+".jpg" }width="100%" />
+          <h4>{props.shoes.title}</h4>
+          <p>{props.shoes.content}</p>
+          <p>{props.shoes.price}</p>
+      </div>
+
+  )
 }
 
 export default App;
---
+++ src/Detail.js
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from 'react'
+import { useHistory, useParams } from 'react-router';
+import styled from 'styled-components'
+import './Detail.scss'
+
+
+let 박스 = styled.div`
+    padding : 20px;
+
+`;
+    
+let 제목 = styled.div`
+    font-size : 25px;
+    color : ${props => props.색상}
+`;
+
+    
+
+function Detail(props){
+
+    useEffect(()=>{
+        // 2초 후에 저거 alert 창을 안보이게 해주셈
+        setTimeout(()=>{2초 후에 저거 alert 창을 안보이게 해주셈}, 2000)
+    })
+
+    let {id} = useParams();
+    let findshoes = props.shoes.find(function(shoes){
+        return shoes.id == id
+    })
+
+
+    let history = useHistory();
+    return(     
+        
+        <div className="container">
+            <박스>
+                <제목 className="red">Detail</제목>
+            </박스>
+            <div className='my-alert2'>
+                <p>재고가 얼마 남지 않았습니다</p>
+            </div>
+
+            <div className="row">
+                <div className="col-md-6">
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                </div>
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">{findshoes.title}</h4>
+                    <p>{findshoes.content}</p>
+                    <p>{findshoes.price}</p>
+                    <button className="btn btn-danger">주문하기</button> 
+                    <button className="btn btn-danger" onClick={()=>{
+                        history.push('/');
+                    }}>뒤로가기</button> 
+                </div>
+            </div>
+        </div> 
+    )
+  }
+
+  export default Detail;
